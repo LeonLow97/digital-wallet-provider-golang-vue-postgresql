87e8bb531239181ed659691f2a90ad570766f0bb
LeonLow97 <lowjiewei@gmail.com>
2023-07-24 22:08:27 +0800

    Added TDD for transactions and transfer components in web

---
+++ web/tests/components/Transactions.spec.js
@@ -0,0 +1,111 @@
+import { shallowMount } from '@vue/test-utils'
+import Transactions from '@/views/Transactions.vue'
+import { describe, it, expect, test, vi } from 'vitest'
+import axios from 'axios'
+
+vi.mock('axios')
+
+describe('Transactions.vue', () => {
+  describe('fetchUsers', () => {
+    test('GET Request, fetches the transaction history', async () => {
+      const transactionsMock = [
+        {
+          date_transferred: '2023-07-02T12:00:00Z',
+          sender_name: 'Alice',
+          amount_transferred: 100,
+          amount_transferred_currency: 'SGD',
+          beneficiary_name: 'Bob',
+          amount_received: 100,
+          amount_received_currency: 'SGD',
+          status: 'CONFIRMED'
+        },
+        {
+          date_transferred: '2023-07-03T12:00:00Z',
+          sender_name: 'Alice',
+          amount_transferred: 100,
+          amount_transferred_currency: 'SGD',
+          beneficiary_name: 'Charlie',
+          amount_received: 74,
+          amount_received_currency: 'USD',
+          status: 'CONFIRMED'
+        }
+      ]
+
+      localStorage.setItem('leon_token', 'JWT_Token')
+
+      axios.get.mockImplementation((url, config) => {
+        expect(url).toBe('http://localhost:4000/transactions')
+        expect(config.headers.Authorization).toBe('Bearer JWT_Token')
+
+        return Promise.resolve({
+          data: {
+            transactions: transactionsMock
+          }
+        })
+      })
+
+      const wrapper = shallowMount(Transactions, {
+        mocks: {
+          localStorage: {
+            getItem: () => 'leon_token'
+          }
+        }
+      })
+
+      await new Promise((resolve) => setTimeout(resolve, 100))
+
+      expect(axios.get).toHaveBeenCalled()
+      expect(wrapper.vm.transactionData).toEqual(transactionsMock)
+    })
+  })
+
+  it('formats date and time correctly', () => {
+    const wrapper = shallowMount(Transactions)
+
+    const formattedDateTime = wrapper.vm.formatDateTime('2023-07-02T12:34:56')
+    expect(formattedDateTime).toEqual('July 2, 2023 at 12:34:56 PM')
+  })
+
+  it('compute transaction data correctly', () => {
+    const transactionData = [
+      {
+        date_transferred: '2023-07-02T12:00:00Z',
+        sender_name: 'Alice',
+        amount_transferred: 100,
+        amount_transferred_currency: 'SGD',
+        beneficiary_name: 'Bob',
+        amount_received: 100,
+        amount_received_currency: 'SGD',
+        status: 'CONFIRMED'
+      },
+      {
+        date_transferred: '2023-07-03T12:00:00Z',
+        sender_name: 'Alice',
+        amount_transferred: 100,
+        amount_transferred_currency: 'SGD',
+        beneficiary_name: 'Charlie',
+        amount_received: 74,
+        amount_received_currency: 'USD',
+        status: 'CONFIRMED'
+      }
+    ]
+
+    const wrapper = shallowMount(Transactions, {
+      data() {
+        return {
+          transactionData
+        }
+      }
+    })
+
+    const computedTransactionData = wrapper.vm.computedTransactionData
+    expect(computedTransactionData).toHaveLength(transactionData.length)
+
+    // Add more expectations for the computed data as needed
+    expect(computedTransactionData[0].sent_amount).toBe('100 SGD')
+    expect(computedTransactionData[0].received_amount).toBe('100 SGD')
+
+    expect(computedTransactionData[1].sent_amount).toBe('100 SGD')
+    expect(computedTransactionData[1].received_amount).toBe('74 USD')
+  })
+})
---
+++ web/tests/components/Transfer.spec.js
@@ -0,0 +1,72 @@
+import { shallowMount } from '@vue/test-utils'
+import Transfer from '@/views/Transfer.vue'
+import { describe, expect, test, vi } from 'vitest'
+import axios from 'axios'
+
+vi.mock('axios')
+
+const fetchBeneficiariesResponse = {
+  data: {
+    beneficiaries: [
+      {
+        beneficiary_id: 2,
+        beneficiary_name: 'Bob',
+        mobile_number: '+65 89230122',
+        currency: 'SGD'
+      },
+      {
+        beneficiary_id: 4,
+        beneficiary_name: 'David',
+        mobile_number: '+49 1234567890',
+        currency: 'EUR'
+      }
+    ]
+  }
+}
+
+const fetchUserResponse = {
+  data: {
+    user: {
+      username: 'Charlie',
+      mobile_number: '+1 555-123-4567',
+      currency: 'USD',
+      balance: '6091.02'
+    }
+  }
+}
+
+describe('Transfer.vue', () => {
+  describe('fetchUser', () => {
+    test('fetches logged in user details', async () => {
+      localStorage.setItem('leon_token', 'JWT_Token')
+
+      axios.get
+        .mockResolvedValueOnce(fetchUserResponse)
+        .mockResolvedValueOnce(fetchBeneficiariesResponse)
+
+      const wrapper = shallowMount(Transfer)
+
+      await new Promise((resolve) => setTimeout(resolve, 100))
+
+      expect(axios.get).toHaveBeenCalledTimes(2)
+      expect(axios.get).toHaveBeenCalledWith('http://localhost:4000/user', expect.any(Object))
+      expect(axios.get).toHaveBeenCalledWith(
+        'http://localhost:4000/beneficiaries',
+        expect.any(Object)
+      )
+
+      expect(wrapper.vm.beneficiaries).toEqual([
+        'Bob,  +65 89230122,  SGD',
+        'David,  +49 1234567890,  EUR'
+      ])
+      expect(wrapper.vm.user).toEqual({
+        user: {
+          username: 'Charlie',
+          mobile_number: '+1 555-123-4567',
+          currency: 'USD',
+          balance: '6091.02'
+        }
+      })
+    })
+  })
+})

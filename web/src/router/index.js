0a6aed742fd52ce5465f507f9a016bb9fe08581e
LeonLow97 <lowjiewei@gmail.com>
2023-07-24 22:04:16 +0800

    Added routes for web in index.js

---
+++ web/src/router/index.js
@@ -0,0 +1,39 @@
+// Composables
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('@/views/Transfer.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import(/* webpackChunkName: "login" */ '@/views/Login.vue')
+  },
+  {
+    path: '/transactions',
+    name: 'transactions',
+    component: () => import('@/views/Transactions.vue')
+  },
+  {
+    path: '/transfer-funds',
+    name: 'Transfer Funds',
+    component: () => import('@/views/Transfer.vue')
+  }
+]
+
+export const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.name !== 'login' && localStorage.getItem('leon_token') === null) {
+    next({ name: 'login' })
+  } else {
+    next()
+  }
+})
+
+export default router

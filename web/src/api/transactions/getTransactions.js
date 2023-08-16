5a595540847842d2fb73b81e0d64bb53ab0c22c5
LeonLow97 <lowjiewei@gmail.com>
2023-08-16 11:43:58 +0800

    Created separate function to call API getTransactions, getUser

---
+++ web/src/api/transactions/getTransactions.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+const getTransactions = async () => {
+  const jwt_token = localStorage.getItem('leon_access_token')
+  const config = {
+    headers: { Authorization: `Bearer ${jwt_token}` }
+  }
+  const url = 'http://localhost:4000/transactions'
+  const response = await axios.get(url, config)
+  console.log(response.data)
+  return response.data
+}
+
+export default getTransactions
---
+++ web/src/api/users/getUser.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const getTransactions = async () => {
+  const jwt_token = localStorage.getItem('leon_access_token')
+  const config = {
+    headers: { Authorization: `Bearer ${jwt_token}` }
+  }
+  const url = 'http://localhost:4000/user'
+  const response = await axios.get(url, config)
+  return response.data
+}
+
+export default getTransactions

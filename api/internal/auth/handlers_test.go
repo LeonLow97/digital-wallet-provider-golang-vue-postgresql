0976625f92ff57f0669cc2bb33fa085856c877d0
LeonLow97 <lowjiewei@gmail.com>
2023-08-29 21:42:47 +0800

    Unit testing Login handler

--- api/internal/auth/handlers_test.go
+++ api/internal/auth/handlers_test.go
@@ -1,7 +1,40 @@
 package auth
 
-import "testing"
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+type MockService struct{}
+
+func (s *MockService) Login(ctx context.Context, creds *Credentials) (*User, *Token, error) {
+	return nil, nil, nil
+}
 
 func Test_Login(t *testing.T) {
-	
+	var mockService *MockService
+
+	authHandler := authHandler{
+		service: mockService,
+	}
+
+	// Create a mock request payload
+	jsonPayload := []byte(`{"username": "testuser", "password": "testpass"}`)
+
+	// generate request
+	req, _ := http.NewRequest(http.MethodPost, "/login", strings.NewReader(string(jsonPayload)))
+	req.Header.Set("Content-Type", "application/json")
+
+	// generate response recorder
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(authHandler.Login)
+
+	handler.ServeHTTP(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("Expected status %d, but got %d", http.StatusOK, rr.Code)
+	}
 }

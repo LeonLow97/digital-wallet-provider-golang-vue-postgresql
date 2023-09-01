0dd2b701acf60072f247c75c0288aa3794ca8847
LeonLow97 <lowjiewei@gmail.com>
2023-09-01 16:34:00 +0800

    Refactoring and improving repo unit testing on GetByUsername

--- api/internal/auth/repo_test.go
+++ api/internal/auth/repo_test.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"testing"
 
@@ -12,8 +13,50 @@ import (
 )
 
 func Test_GetByUsername(t *testing.T) {
+	testCases := []struct {
+		Test         string
+		Username     string
+		ExpectedUser *User
+		ExpectErr    bool
+		QueryExpect  func(mock sqlmock.Sqlmock)
+	}{
+		{
+			Test:     "Valid User",
+			Username: "validUsername",
+			ExpectedUser: &User{
+				ID:       1,
+				Username: "validUsername",
+				Password: "validPassword",
+				Active:   1,
+				Admin:    1,
+			},
+			ExpectErr: false,
+			QueryExpect: func(mock sqlmock.Sqlmock) {
+				// Define the expected rows that will be returned by the query
+				rows := sqlmock.NewRows([]string{"id", "username", "password", "active", "admin"}).
+					AddRow(1, "validUsername", "validPassword", 1, 1)
+
+				mock.ExpectQuery("SELECT id, username, password, active, admin FROM users WHERE username = $1").
+					WithArgs("validUsername").
+					WillReturnRows(rows)
+			},
+		},
+		{
+			Test:         "Non-Existent User",
+			Username:     "invalidUsername",
+			ExpectedUser: nil,
+			ExpectErr:    true,
+			QueryExpect: func(mock sqlmock.Sqlmock) {
+				mock.ExpectQuery("SELECT id, username, password, active, admin FROM users WHERE username = $1").
+					WithArgs("invalidUsername").
+					WillReturnError(sql.ErrNoRows)
+			},
+		},
+	}
+
 	// Create a mock database connection using sqlmock
-	mockDB, mock, err := sqlmock.New()
+	// mockDB, mock, err := sqlmock.New() // less restricted query string
+	mockDB, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
 	if err != nil {
 		t.Fatalf("unexpected error when opening a stub database connection: %s", err)
 	}
@@ -22,31 +65,25 @@ func Test_GetByUsername(t *testing.T) {
 	// Convert the mock database connection to an *sqlx.DB instance
 	sqlxDB := sqlx.NewDb(mockDB, "sqlmock")
 
-	// Define the expected rows that will be returned by the query
-	rows := sqlmock.NewRows([]string{"id", "username", "password", "active", "admin"}).
-		AddRow(1, "testUsername", "testPassword", 1, 1)
-
-	// Set up the expectation for the query using the mock
-	// mockDB (now sqlxDB) then responds with the rows defined in `NewRows(...).AddRow(...)`
-	// Thus, we can simulate the behavior of the database without interacting with a real database
-	mock.ExpectQuery("SELECT id, username, password, active, admin FROM users").WithArgs("testUsername").WillReturnRows(rows)
-
 	// Create a repository instance using the mock database connection
 	r, err := NewRepo(sqlxDB)
 	require.NoError(t, err, "creating the shared repo")
 
-	// Call the GetByUsername method on the repository
-	results, err := r.GetByUsername(context.Background(), "testUsername")
-	require.NoError(t, err, "running GetByUsername on repository instance")
+	for _, tc := range testCases {
+		t.Run(tc.Test, func(t *testing.T) {
+			// generating the expected query results
+			tc.QueryExpect(mock)
 
-	expectedResult := &User{
-		ID:       1,
-		Username: "testUsername",
-		Password: "testPassword",
-		Active:   1,
-		Admin:    1,
+			// call the GetByUsername method in the repository layer
+			results, err := r.GetByUsername(context.Background(), tc.Username)
+
+			if !tc.ExpectErr {
+				require.NoError(t, err, "running GetByUsername on repository layer")
+				assert.Equal(t, fmt.Sprintf("%+v", results), fmt.Sprintf("%+v", tc.ExpectedUser))
+			} else {
+				require.Error(t, err, "running GetByUsername on repository layer")
+				assert.Equal(t, results, tc.ExpectedUser)
+			}
+		})
 	}
-
-	// Compare the expected result with the actual result using assertions
-	assert.Equal(t, fmt.Sprintf("%+v", expectedResult), fmt.Sprintf("%+v", results))
 }

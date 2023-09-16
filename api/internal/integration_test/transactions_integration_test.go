b84ede52532c8dd39bd94848fd9505485ddabdf9
LeonLow97 <lowjiewei@gmail.com>
2023-09-16 15:46:47 +0800

    created integration test for GetUserCountByUserId and GetUserIdByMobileNumber

---
+++ api/internal/integration_test/transactions_integration_test.go
@@ -0,0 +1,51 @@
+package integration_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/LeonLow97/internal/transactions"
+	"github.com/stretchr/testify/require"
+	"gotest.tools/v3/assert"
+)
+
+func TestIntegration_GetUserCountById(t *testing.T) {
+	testRepo, err := transactions.NewRepo(testDB)
+	if err != nil {
+		require.NoError(t, err)
+	}
+
+	count, err := testRepo.GetUserCountByUserId(context.Background(), 1)
+	if err != nil {
+		require.NoError(t, err)
+	}
+
+	assert.Equal(t, count, 1)
+
+	count, err = testRepo.GetUserCountByUserId(context.Background(), 999)
+	if err != nil {
+		require.NoError(t, err)
+	}
+	assert.Equal(t, count, 0)
+}
+
+func TestIntegration_GetUserIdByMobileNumber(t *testing.T) {
+	testRepo, err := transactions.NewRepo(testDB)
+	if err != nil {
+		require.NoError(t, err)
+	}
+
+	userId, err := testRepo.GetUserIdByMobileNumber(context.Background(), "+44 7712345678")
+	if err != nil {
+		require.NoError(t, err)
+	}
+
+	assert.Equal(t, userId, 4)
+
+	userId, err = testRepo.GetUserIdByMobileNumber(context.Background(), "1")
+	if err != nil {
+		require.NoError(t, err)
+	}
+
+	assert.Equal(t, userId, 0)
+}

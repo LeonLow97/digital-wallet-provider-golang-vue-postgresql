7be5ed4c90e960f73e617569647d95dd61da4d8f
LeonLow97 <lowjiewei@gmail.com>
2024-01-20 16:18:37 +0800

    Removed client legacy code

--- client_legacy/tests/unit/utils/formatDateTime.test.js
+++
@@ -1,9 +0,0 @@
-import formatDateTime from "@/utils/formatDateTime";
-
-describe("formatDateTime", () => {
-    it("formats the date time correct in en-US format", () => {
-        const dateTime = "2023-08-16T03:33:33.730676Z"
-
-        expect(formatDateTime(dateTime)).toBe("August 16, 2023 at 11:33:33 AM")
-    })
-})

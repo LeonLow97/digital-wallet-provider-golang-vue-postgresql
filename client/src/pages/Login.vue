bc88cb7f9ded16c70e18d55c60360f6932c6095e
LeonLow97 <lowjiewei@gmail.com>
2024-02-25 12:38:38 +0800

    Styling Login page

--- client/src/pages/Login.vue
+++ client/src/pages/Login.vue
@@ -1,37 +1,118 @@
 <template>
-  <div>
-    <h1>Login Page</h1>
+  <div class="login_container">
+    <h1>Login</h1>
     <form @submit.prevent="handleSubmit">
-      <label for="email">Email</label>
-      <text-input id="email" v-model.trim="email" placeholder="Email" />
-      <label for="password">Password</label>
-      <text-input
-        id="password"
-        v-model.trim="password"
-        placeholder="Password"
-        :type="showPassword ? 'text' : 'password'"
-      />
-      <input type="checkbox" v-model="showPassword" />
-      <action-button text="Login" />
+      <div>
+        <label for="email">Email</label>
+        <text-input
+          class="text_input"
+          id="email"
+          v-model.trim="email"
+          placeholder="leonlow@example.com"
+        />
+      </div>
+
+      <div>
+        <label for="password">Password</label>
+        <div class="text_input-container">
+          <text-input
+            class="text_input"
+            id="password"
+            v-model.trim="password"
+            placeholder="Password"
+            :type="showPassword ? 'text' : 'password'"
+          />
+          <span class="icon">
+            <svg-icon
+              v-model="showPassword"
+              type="mdi"
+              :path="showPassword ? mdiEyeOutline : mdiEyeOffOutline"
+              @click="togglePasswordVisibility"
+            ></svg-icon>
+          </span>
+        </div>
+      </div>
+
+      <div class="button_container">
+        <action-button text="Login" />
+      </div>
     </form>
     <router-link :to="{ name: 'SignUp' }"
       >New here? Click to create an account!</router-link
     >
-    <span>Temporary message: {{ responseMessage }}</span>
   </div>
 </template>
 
+<style scoped>
+.login_container {
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+  padding: 4.8rem;
+  border-radius: 2.4rem;
+  box-shadow: 0 1.2rem 2.4rem rgba(0, 0, 0, 0.2);
+}
+
+.text_input {
+  border-top: none;
+  border-left: none;
+  border-right: none;
+  padding: 1.2rem 1rem 1.2rem 1rem;
+  font-size: 1.8rem;
+}
+
+.text_input-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+}
+
+.text_input-container .text_input {
+  width: 90%;
+}
+
+.button_container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+  width: 40rem;
+  gap: 2.8rem;
+}
+
+form div {
+  display: flex;
+  flex-direction: column;
+}
+
+.icon {
+  cursor: pointer;
+}
+</style>
+
 <script lang="ts" setup>
 import { ref } from 'vue';
-import TextInput from '@/components/TextInput.vue';
-import ActionButton from '@/components/ActionButton.vue';
+import { mdiEyeOutline, mdiEyeOffOutline } from '@mdi/js';
+import SvgIcon from '@jamescoyle/vue-icon';
 import { AxiosError } from 'axios';
 import { useRouter } from 'vue-router';
 import { useUserStore } from '@/stores/user';
+
 import type { User } from '@/types/user';
+import TextInput from '@/components/TextInput.vue';
+import ActionButton from '@/components/ActionButton.vue';
 import postLogin from '@/api/user';
 
 const showPassword = ref(false);
+
+const togglePasswordVisibility = () => {
+  showPassword.value = !showPassword.value;
+};
 
 const email = ref('');
 const password = ref('');
